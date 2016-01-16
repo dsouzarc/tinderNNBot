1098fd40b75fb752ce9cdf750076f92feebbbc18
Ryan D'souza <dsouzarc@gmail.com>
2016-01-16 18:35:11 -0500

    Encode/decode methods + swipe status

--- Person.py
+++ Person.py
@@ -6,29 +6,37 @@ class Person:
     name = None;
     photos = None;
     personID = None;
-    swiped = None;
+    swiped = None; # -1 = No swipe, 0 = passed, 1 = like, 2 = super like
 
-    def __init__(self, jsonData):
+    def __init__(self, jsonData, personJSON=None):
         
-        self.jsonData = jsonData;
-        self.name = jsonData["name"];
-        self.personID = jsonData["_id"];
-        self.photos = [];
+        if personJSON is None:
+            self.jsonData = jsonData;
+            self.name = jsonData["name"];
+            self.personID = jsonData["_id"];
+            self.swiped = -1;
+            self.photos = [];
 
-        for photo in jsonData["photos"]:
-            if "processedFiles" in photo:
-                processedImages = photo["processedFiles"];
-                for processedImage in processedImages:
-                    if processedImage["width"] == 640 or processedImage["height"] == 640:
-                        self.photos.append(processedImage["url"]);
-            else:
-                self.photos.append(photo['url']);
+            for photo in jsonData["photos"]:
+                if "processedFiles" in photo:
+                    processedImages = photo["processedFiles"];
+                    for processedImage in processedImages:
+                        if processedImage["width"] == 640 or processedImage["height"] == 640:
+                            self.photos.append(processedImage["url"]);
+                else:
+                    self.photos.append(photo['url']);
+        else:
+            self.name = personJSON["name"];
+            self.photos = personJSON["photos"];
+            self.personID = personJSON["personID"];
+            self.swiped = personJSON["swiped"];
 
     def getPersonJSON():
         data = {
             "name": self.name,
             "photos" : self.photos,
             "personID" : self.personID,
+            "swiped": self.swiped,
             "data": self.jsonData
         };
         return data;
