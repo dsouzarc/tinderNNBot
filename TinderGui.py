2450e27a9b30ec22f3d070ebe7ff4e2cb8ceffca
Ryan D'souza <dsouzarc@gmail.com>
2016-01-28 01:48:57 -0500

    Began O-O design + UI

--- TinderGui.py
+++ TinderGui.py
@@ -1,19 +1,28 @@
 import sys
 from PyQt4 import QtGui
 
+class TinderGui(QtGui.QWidget):
+
+    def __init__(self):
+        super(TinderGui, self).__init__();
+
+        self.initUI();
+
+    def initUI(self):
+        #x, y, width, height
+        self.setGeometry(300, 300, 250, 150);
+        self.setWindowTitle('Tinder Bot');
+        #self.setWindowIcon(QTGui.QIcon('web.png'));
+
+        self.show();
+
 
 def main():
-    
-    app = QtGui.QApplication(sys.argv)
 
-    w = QtGui.QWidget()
-    w.resize(250, 150)
-    w.move(300, 300)
-    w.setWindowTitle('Simple')
-    w.show()
-    
-    sys.exit(app.exec_())
-
+    app = QtGui.QApplication(sys.argv);
+    tinderGui = TinderGui();
+    sys.exit(app.exec_());
 
 if __name__ == '__main__':
-    main()
+    main();
+   
