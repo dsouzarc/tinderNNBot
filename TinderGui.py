bd0a7c15e73cae6f12faa97709613c196591bbe7
Ryan D'souza <dsouzarc@gmail.com>
2016-01-29 01:50:20 -0500

    Began O-O design + UI

--- TinderGui.py
+++ TinderGui.py
@@ -9,6 +9,15 @@ class TinderGui(QtGui.QWidget):
         self.initUI();
 
     def initUI(self):
+        QtGui.QToolTip.setFont(QtGui.QFont('SansSerif', 10))
+        
+        self.setToolTip('This is a <b>QWidget</b> widget')
+        
+        btn = QtGui.QPushButton('Button', self)
+        btn.setToolTip('This is a <b>QPushButton</b> widget')
+        btn.resize(btn.sizeHint())
+        btn.move(50, 50)       
+        
         #x, y, width, height
         self.setGeometry(300, 300, 250, 150);
         self.setWindowTitle('Tinder Bot');
