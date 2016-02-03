8917a46e2c740bd005ec52a3d82a325f8e12e82f
Ryan D'souza <dsouzarc@gmail.com>
2016-02-02 19:32:45 -0500

    Code cleanup + supports going through multiple photos

--- TinderGui.py
+++ TinderGui.py
@@ -1,6 +1,6 @@
 import sys;
 import urllib;
-from PyQt4 import QtGui;
+from PyQt4 import QtGui, QtCore
 
 from Tinder import Tinder;
 from Person import Person;
@@ -9,55 +9,95 @@ from Person import Person;
 class TinderGui(QtGui.QWidget):
 
     tinder = None;
+    recommendations = None;
 
+    currentImageLabel = None;
     swipeRightButton = None;
     swipeLeftButton = None;
+    photoIndex = None;
 
     def __init__(self):
         super(TinderGui, self).__init__();
 
+        self.photoIndex = 0;
         self.tinder = Tinder(fileName="credentials.json");
+        self.recommendations = self.tinder.getRecommendations();
+
+        if len(self.recommendations) == 0:
+            print("0 RECOMMENDATIONS")
+            return;
+
         self.initUI();
 
     def initUI(self):
 
-        self.swipeRightButton = QtGui.QPushButton('Right', self);
-        self.swipeLeftButton = QtGui.QPushButton('Left', self);
-        self.swipeRightButton.clicked.connect(self.swipeRight);
-        self.swipeLeftButton.clicked.connect(self.swipeLeft);
+        self.createUIComponents();
+        self.displayPhoto();
 
-        recommendations = self.tinder.getRecommendations();
+        mainLayout = QtGui.QHBoxLayout()
+        mainLayout.addStretch(1)
 
-        #url = recommendations[0].photos[0];
-        url = "http://iconshow.me/media/images/ui/ios7-icons/png/512/person_1.png"
+        mainLayout.addWidget(self.swipeLeftButton);
+        mainLayout.addWidget(self.swipeRightButton);
+        mainLayout.addWidget(self.currentImageLabel)
+
+        self.setLayout(mainLayout)
+
+        print("NUM PHOTOS: " + str(len(self.recommendations[0].photos)))
+
+        self.setWindowTitle('Tinder Bot');
+        self.setWindowIcon(QtGui.QIcon('tinder_icon.png'));
+        self.resize(500, 500);
+        self.center();
+        self.show()
+        
+    def keyPressEvent (self, eventQKeyEvent):
+        QtGui.QWidget.keyPressEvent(self, eventQKeyEvent)
+
+        key = eventQKeyEvent.key();
+
+        print("HERE: " + str(self.photoIndex))
+
+        if key == QtCore.Qt.Key_Left:
+            self.swipeLeft();
+        elif key == QtCore.Qt.Key_Right:
+            self.swipeRight();
+
+        elif key == QtCore.Qt.Key_Up:
+            self.photoIndex -= 1;
+            if self.photoIndex < 0:
+                self.photoIndex = len(self.recommendations[0].photos) - 1;
+            self.displayPhoto();
+
+        elif key == QtCore.Qt.Key_Down:
+            self.photoIndex += 1;
+            if self.photoIndex >= len(self.recommendations[0].photos):
+                self.photoIndex = 0;
+            self.displayPhoto();
+
+
+    def displayPhoto(self):
+        url = self.recommendations[0].photos[self.photoIndex];
         data = urllib.urlopen(url).read();
 
         image = QtGui.QImage()
         image.loadFromData(data)
 
-        lbl = QtGui.QLabel(self)
-        lbl.setPixmap(QtGui.QPixmap(image))
+        self.currentImageLabel.setPixmap(QtGui.QPixmap(image))
 
-        mainLayout = QtGui.QHBoxLayout()
-        mainLayout.addStretch(1)
-        mainLayout.addWidget(self.swipeLeftButton);
-        mainLayout.addWidget(self.swipeRightButton);
+    def createUIComponents(self):
+        self.currentImageLabel = QtGui.QLabel(self)
 
-        mainLayout.addWidget(lbl)
+        self.swipeRightButton = QtGui.QPushButton('Right', self);
+        self.swipeLeftButton = QtGui.QPushButton('Left', self);
 
-        vbox = QtGui.QVBoxLayout()
-        vbox.addStretch(1)
-        #vbox.addLayout(mainLayout)
-        self.setLayout(mainLayout)
+        self.swipeRightButton.clicked.connect(self.swipeRight);
+        self.swipeLeftButton.clicked.connect(self.swipeLeft);
 
-        self.setWindowTitle('Tinder Bot');
-        self.setWindowIcon(QtGui.QIcon('tinder_icon.png'));
-        self.resize(250, 150);
-        self.center();
-        self.show()
 
     def swipeRight(self):
         print("Swiped right");
+
     def swipeLeft(self):
         print("Swiped left");
 
