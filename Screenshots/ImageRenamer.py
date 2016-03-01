b5eb151f3061ec85485bac6a354697e5a1d11879
Ryan D'souza <dsouzarc@gmail.com>
2016-03-01 10:35:35 -0500

    Added Screenshots

---
+++ Screenshots/ImageRenamer.py
@@ -0,0 +1,33 @@
+import os;
+
+
+#Written by Ryan Dsouza
+#Quickly renames all the photos in a directory with the format
+#'Screenshot_X.png', where X is that file's place in the directory
+
+#Also prints out the markdown code to display these many images
+
+#Run instructions: 'python ImageRenamer.py'
+
+
+files = os.listdir('.');
+index = 0;
+
+projectName = "groupMeForWatch"
+
+prefix = "https://github.com/dsouzarc/" + projectName + "/blob/master/Screenshots/";
+
+for fileName in sorted(files):
+
+    if fileName != "ImageRenamer.py":
+
+        if "Screenshot_" in fileName:
+            index += 1;
+
+for fileName in sorted(files):
+        if "Screenshot_" not in fileName and fileName != "ImageRenamer.py":
+            newFileName = "Screenshot_" + str(index) + ".png";
+            os.rename(fileName, newFileName);
+            print("![Screenshot " + str(index) + "](" + prefix + newFileName + ")");
+            index += 1;
+
