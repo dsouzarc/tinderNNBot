f5faf7d90a0491fe11818774957af622f48c9840
Ryan D'souza <dsouzarc@gmail.com>
2016-03-05 06:45:31 -0500

    Tinder API getting request issues

--- Tinder.py
+++ Tinder.py
@@ -31,7 +31,7 @@ class Tinder:
         self.headers = self.generateHeaders();
 
         #If we don't have a Tinder token
-        if len(self.tinderToken) == 0 or len(self.tinderAPIToken) == 0:
+        if len(self.tinderToken) == 0 or len(self.tinderAPIToken) == 0 or 1 == 1:
             self.login();
         
         #If our tindertoken is expired
@@ -130,6 +130,10 @@ class Tinder:
         url = "https://api.gotinder.com/user/recs?locale=en-US";
         result = self.session.post(url,headers=self.headers, proxies=None);
 
+        print(json.dumps(self.headers, indent=4))
+
+        print("RESULT IS: " + str(result))
+
         if result.status_code == 200:
             result = result.json();
             if result["status"] == 200:
@@ -157,18 +161,19 @@ class Tinder:
         };
         headers = {
             "Host": "api.gotinder.com",
+            "host": "api.gotinder.com",
             "Accept": "*/*",
-            "app-version": "467",
-            "x-client-version": "47217",
+            "app-version": "535",
+            "x-client-version": "48222",
             "Proxy-Connection": "keep-alive",
             "Accept-Encoding": "gzip, deflate",
             "Accept-Language": "en-US;q=1",
             "platform": "ios",
             "Facebook-ID": self.facebookID,
-            "User-Agent": "Tinder/4.7.2 (iPhone; iOS 9.2; Scale/2.00)",
-            "If-None-Match": "2020547529",
+            "User-Agent": "Tinder/4.8.2 (iPhone; iOS 9.2.1; Scale/2.00)",
+            "If-None-Match": 'W/"2004809701"',
             "Connection": "keep-alive",
-            "os_version": "900002",
+            "os_version": "90000200001",
             "Content-Type": "application/json"
         };
 
@@ -177,10 +182,15 @@ class Tinder:
 
         if result.status_code == 200:
             result = result.json();
-            headers["X-Auth-Token"] = self.tinderToken;
             self.tinderToken = result["token"];
+            print("OUR TOKEN: " + str(self.tinderToken))
             self.tinderAPIToken = result["user"]["api_token"];
             self.myName = result["user"]["full_name"];
+
+            headers["X-Auth-Token"] = self.tinderToken;
+            headers["Authorization"] = 'Token token="%s"' % self.tinderToken
+            print(headers["Authorization"])
+
             self.headers = headers;
             self.session.headers.update(headers);
             print("Successful logged " + self.myName + " in with Tinder Token: " + self.tinderToken + "\tAPI Token: " + self.tinderAPIToken);
@@ -212,3 +222,8 @@ class Tinder:
             "Content-Type": "application/json charset=utf-8"
         };
         return headers;
+
+
+if __name__ == "__main__":
+    tinder = Tinder(fileName="credentials.json");
+    print(tinder.getRecommendations())
