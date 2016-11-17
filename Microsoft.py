26c75df9894d2b05d23225c24c2288ae63599779
Ryan D'souza <dsouzarc@gmail.com>
2016-11-16 21:38:55 -0500

    Prints emotions based off of Microsoft's Emotion API

--- Microsoft.py
+++ Microsoft.py
@@ -12,15 +12,43 @@ class Microsoft:
     #Dictionary of credentials
     credentials = None
 
+    session = None
+
     '''
     Constructor
     '''
     def __init__(self, fileName="credentials.json"):
 
+        self.session = requests.session()
         self.credentials = json.load(open(fileName))
-        print(self.credentials["microsoft_emotion_key1"])
+
+
+    def getEmotions(self, linkToImage):
+        
+        headers = {
+            "Content-Type": "application/json",
+            "Ocp-Apim-Subscription-Key": self.credentials["microsoft_emotion_key1"]
+        }
+
+        body = {
+            "url": linkToImage
+        }
+
+        url = "https://api.projectoxford.ai/emotion/v1.0/recognize"
+
+
+        response = self.session.post(url, headers=headers, json=body)
+
+        print(response.json())
+
+
+
+
+
 
 microsoft = Microsoft(fileName="credentials.json")
+
+microsoft.getEmotions("https://scontent-lga3-1.xx.fbcdn.net/t31.0-8/14876465_10210383671844847_1308604206555951240_o.jpg")
 
 
 
