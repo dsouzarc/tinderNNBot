646f01c652440861c99f419c4500753608dc1ecb
Ryan D'souza <dsouzarc@gmail.com>
2016-11-18 13:24:35 -0500

    Return objects

--- Microsoft.py
+++ Microsoft.py
@@ -3,6 +3,30 @@ import json
 import sys
 
 class Microsoft:
+    class FacialEmotion:
+
+        jsonResponse = None
+
+        def __init__(self, jsonResponse):
+            self.jsonResponse = jsonResponse
+
+        def getDimensions(self):
+            dim = self.jsonResponse["faceRectangle"]
+            return (dim["height"], dim["left"], dim["top"], dim["width"])
+
+        def getMainEmotion(self):
+            emotions = self.jsonResponse["scores"]
+
+            highestEmotion = emotions.keys()[0]
+            highestScore = emotions[highestEmotion]
+
+            for emotion in emotions.keys():
+                if emotions[emotion] > highestScore:
+                    highestEmotion = emotion
+                    highestScore = emotions[emotion]
+
+            return (highestEmotion, highestScore)
+
 
     '''
     Handles API calls to Microsoft Cognitive Services API
@@ -35,20 +59,25 @@ class Microsoft:
         }
 
         url = "https://api.projectoxford.ai/emotion/v1.0/recognize"
+        response = self.session.post(url, headers=headers, json=body).json()
 
 
-        response = self.session.post(url, headers=headers, json=body)
+        if len(response) != 0:
+            for face in response:
+                f = Microsoft.FacialEmotion(face)
+                print(f.getDimensions())
+                print(f.getMainEmotion())
+            print("NOT 0")
 
-        print(response.json())
-
-
+        return response
 
 
 
 
 microsoft = Microsoft(fileName="credentials.json")
 
-microsoft.getEmotions("https://scontent-lga3-1.xx.fbcdn.net/t31.0-8/14876465_10210383671844847_1308604206555951240_o.jpg")
+faces = microsoft.getEmotions("https://scontent-lga3-1.xx.fbcdn.net/t31.0-8/14876465_10210383671844847_1308604206555951240_o.jpg")
+
 
 
 
