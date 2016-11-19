f4915c14919f12fb34119bb95ad11ad1e2fa6994
Ryan D'souza <dsouzarc@gmail.com>
2016-11-18 22:14:49 -0500

    Comments + code cleanup + testing

--- Microsoft.py
+++ Microsoft.py
@@ -5,7 +5,20 @@ import sys
 from StringIO import StringIO
 from PIL import Image, ImageDraw, ImageFont
 
+######################################################################
+#
+#                   Facial Emotion - API return object
+#                   
+######################################################################
+
+
 class FacialEmotion:
+
+    '''
+    Represents the object returned from an API call to Microsoft's Emotions API
+    Includes the dimensions/coordinates of identified faces
+    And the strengths of emotions for each face
+    '''
 
     jsonResponse = None
 
@@ -30,6 +43,15 @@ class FacialEmotion:
 
         return (highestEmotion, highestScore)
 
+
+
+######################################################################
+#
+#                   Microsoft - API Client
+#                   
+######################################################################
+
+
 class Microsoft:
 
 
@@ -52,6 +74,10 @@ class Microsoft:
         self.credentials = json.load(open(fileName))
 
 
+    '''
+    Makes the API call to Microsoft's Emotions API
+    Returns an array of FacialEmotion objects
+    '''
     def getEmotions(self, linkToImage):
         
         headers = {
@@ -72,62 +98,68 @@ class Microsoft:
             for face in response:
                 f = FacialEmotion(face)
                 facialEmotions.append(f)
-                print(f.getDimensions())
-                print(f.getMainEmotion())
-            print("NOT 0")
 
         return facialEmotions
 
     
-    def drawFacesAndEmotions(self, pathToImage, image=None):
+    '''
+    Responsible for drawing the rectangles around faces and emotions on an image
+    If those values are not supplied, makes the appropriate calls to get them
+    '''
+    def drawFacesAndEmotions(self, pathToImage, image=None, facialEmotions=None):
 
         #If we aren't supplied an image, download it
         if image is None:
             image = Image.open(StringIO(requests.get(pathToImage).content))
 
+        #If we aren't supplied the facial emotions, get it
+        if facialEmotions is None:
+            facialEmotions = self.getEmotions(pathToImage)
 
-        
+        #For drawing lines and text on the image
+        draw = ImageDraw.Draw(image)
 
+        for facialEmotion in facialEmotions:
+            face = facialEmotion.getDimensions()
 
+            lineFill = 500
+            lineWidth = 5
 
+            faceX0 = face["left"]
+            faceX1 = face["left"] + face["width"]
+            faceY0 = face["top"]
+            faceY1 = face["top"] + face["height"]
 
-microsoft = Microsoft(fileName="credentials.json")
+            #Left to right side of face, top 
+            draw.line((faceX0, faceY0, faceX1, faceY0), width=lineWidth, fill=lineFill)
 
-facialEmotions = microsoft.getEmotions("https://scontent-lga3-1.xx.fbcdn.net/t31.0-8/14876465_10210383671844847_1308604206555951240_o.jpg")
-microsoft.drawFacesAndEmotions("https://scontent-lga3-1.xx.fbcdn.net/t31.0-8/14876465_10210383671844847_1308604206555951240_o.jpg")
+            #Left to right side of face, bottom 
+            draw.line((faceX0, faceY1, faceX1, faceY1), width=lineWidth, fill=lineFill)
 
+            #Left side of face, top to bottom
+            draw.line((faceX0, faceY0, faceX0, faceY1), width=lineWidth, fill=lineFill)
 
-im = Image.open("/Users/Ryan/Downloads/14876465_10210383671844847_1308604206555951240_o.jpg")
+            #Right side of face, top to bottom
+            draw.line((faceX1, faceY0, faceX1, faceY1), width=lineWidth, fill=lineFill)
 
+            #Draw the main emotion
+            mainEmotion = facialEmotion.getMainEmotion()
+            draw.text((max(0, faceX0 - 60), max(0, faceY0 - 40)), mainEmotion[0], font=ImageFont.truetype("Verdana.ttf",34), fill=lineFill)
 
-
-draw = ImageDraw.Draw(im)
-
-for facialEmotion in facialEmotions:
-    face = facialEmotion.getDimensions()
-
-    lineFill = 500
-    lineWidth = 5
-
-    faceX0 = face["left"]
-    faceX1 = face["left"] + face["width"]
-    faceY0 = face["top"]
-    faceY1 = face["top"] + face["height"]
-
-    #Left to right side of face, top 
-    draw.line((faceX0, faceY0, faceX1, faceY0), width=lineWidth, fill=lineFill)
-
-    #Left to right side of face, bottom 
-    draw.line((faceX0, faceY1, faceX1, faceY1), width=lineWidth, fill=lineFill)
-
-    #Left side of face, top to bottom
-    draw.line((faceX0, faceY0, faceX0, faceY1), width=lineWidth, fill=lineFill)
-
-    #Right side of face, top to bottom
-    draw.line((faceX1, faceY0, faceX1, faceY1), width=lineWidth, fill=lineFill)
-
-    mainEmotion = facialEmotion.getMainEmotion()
-    draw.text((max(0, faceX0 - 60), max(0, faceY0 - 40)), mainEmotion[0], font=ImageFont.truetype("Verdana.ttf",34), fill=lineFill)
+        #Return our image with everything drawn
+        return image
 
 
-im.show()
+
+######################################################################
+#
+#                   Main method - testin
+#                   
+######################################################################
+
+if __name__ == "__main__":
+
+    microsoft = Microsoft(fileName="credentials.json")
+
+    image = microsoft.drawFacesAndEmotions("https://scontent-lga3-1.xx.fbcdn.net/t31.0-8/14876465_10210383671844847_1308604206555951240_o.jpg")
+    image.show()
